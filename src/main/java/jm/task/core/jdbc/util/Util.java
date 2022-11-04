243cf31a6abdc78e8bf59a747ad7e807c2cd1a49
andretti <andretti300585@gmail.com>
2022-11-04 20:27:33 +0700

    mycommit

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,33 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static Util util;
+    private Util() {}
+
+    /*public synchronized Util getUtil() {
+        if (util == null) {
+            util = new Util();
+        }
+        return util;
+    }*/
+    public static Connection getMySQLConnection() throws SQLException,
+            ClassNotFoundException {
+        String hostName = "localhost";
+        String dbName = "alfa";
+        String userName = "root";
+        String password = "wendows";
+        String connectionURL = "jdbc:mysql://" + hostName + ":3306/" + dbName;
+
+
+        Connection conn = DriverManager.getConnection(connectionURL, userName,
+                password);
+
+        return conn;
+
+    }
 }
